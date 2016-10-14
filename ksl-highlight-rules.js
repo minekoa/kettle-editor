9fe3b64cfc5c5ac957b461f1b62166dacf001814
minekoa <mineko.orange@gmail.com>
2016-10-14 20:06:12 +0900

    KSLのシンタックスハイライトルールを記述。色分けだけはそれぽく。

--- ksl-highlight-rules.js
+++ ksl-highlight-rules.js
@@ -6,8 +6,47 @@ ace.define('ace/mode/ksl_highlight_rules', function(require, exports, module) {
     const oop = require("ace/lib/oop");
     const {TextHighlightRules} = require("ace/mode/text_highlight_rules");
 
-    let KslHighlightRules = function() {
-        this.$rules = new TextHighlightRules().getRules();
+    var KslHighlightRules = function() {
+        const keywords = (
+            'var device'
+        );
+        const keyword_controls = (
+            'skip if then else while do wait until after parallel'
+        );
+        const builtin_functions = (
+                  'assert log print get_time diff_time_ms '
+                + 'average max min abs '
+                + 'target_frame target_frame_a target_joints '
+                + 'move_end_effector_position move_end_effector_position_n ' 
+                + 'move_joints_position move_joints_position_n '
+                + 'stop_joints '
+        );
+        const operators = (
+                  "\\+|\\-|\\*|\\/|"             // + - * /
+                + "\\<|\\>|\\<\\=|\\>\\=|"       // < > <= >=
+                + "\\=\\=|\\!\\=|"               // == !=
+                + "\\&\\&|\\|\\||"               // && ||
+                + "\\="                          // =
+        );
+        let keywordMapper = this.$keyword = this.createKeywordMapper(
+            { 'keyword'         : keywords,
+              'keyword.control' : keyword_controls,
+              'support.function': builtin_functions },
+            'identifier', false, ' '
+        );
+
+        this.$rules = {
+            "start": [
+                { token: "comment"         , regex: "\\/\\/.*$" },
+                { token: "constant.numeric", regex: "[+-]?([\\d]*\\.?[\\d]+|[\\d]+\\.)" },
+                { token: "string"          , regex: "[\"][^\"][\"]" },
+                { token: keywordMapper     , regex: "[a-zA-Z_][a-zA-Z0-9_]*\\b" },
+                { token: "keyword.operator", regex: operators },
+                { token: "paren.lparen"    , regex: "[[({]" },
+                { token: "paren.rparen"    , regex: "[\\])}]" },
+                { caseInsentitive: false, defaultToken: "invalid" }
+            ]
+        }
     };
 
     oop.inherits(KslHighlightRules, TextHighlightRules);
